57aee476cb31fbba0f5cf14510a1cc95281f0253
AhmadWahyuApryanto <ahmadwahyuapryanto18@gmail.com>
2026-05-06 16:08:08 +0700

    update vercel

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\URL; // 1. Kita panggil fungsi URL dari Laravel
+use Illuminate\Support\Facades\URL; // Tambahkan pemanggilan URL Facade
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,8 +20,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        // 2. Jika aplikasi kita BUKAN di komputer lokal, paksa gunakan HTTPS
-        if (env('APP_ENV') !== 'local') {
+        // Paksa aplikasi menggunakan HTTPS jika berjalan di Vercel/Production
+        if(config('app.env') !== 'local') {
             URL::forceScheme('https');
         }
     }
