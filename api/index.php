4b3611d726dbd312cf6c2b00a1b1f62a791b2dae
AhmadWahyuApryanto <ahmadwahyuapryanto18@gmail.com>
2026-05-06 07:33:05 +0700

    ubah vercel dan db supabase

---
+++ api/index.php
@@ -0,0 +1,5 @@
+<?php
+
+// File ini berfungsi untuk meneruskan semua permintaan masuk dari Vercel
+// menuju ke file eksekusi utama Laravel yang ada di folder public.
+require __DIR__ . '/../public/index.php';
